c4af6f1b535ad14a265671476b209c919040b68f
Heehong Moon <bbirec@gmail.com>
2013-11-04 22:35:07 +0900

    Enhanced precision.

--- make.sh
+++ make.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-# Simplifying Proportion(5%)
-PROPORTION=0.05
+# Simplifying Proportion(10%)
+QUANTIZATION=1e5
+PROPORTION=0.10
 
 # Simplify the original ESRI Shapefile and generates TopoJSON.
 # Map the *_name, *_code columns into name, code.
-topojson --simplify-proportion $PROPORTION -o topojson/regions.topojson -p code=state_code,name=state_name,code=city_code,name=city_name,code=dong_code,name=dong_name --shapefile-encoding utf8 -- shp/state.shp shp/city.shp shp/dong.shp
+topojson -q $QUANTIZATION --simplify-proportion $PROPORTION -o topojson/regions.topojson -p code=state_code,name=state_name,code=city_code,name=city_name,code=dong_code,name=dong_name --shapefile-encoding utf8 -- shp/state.shp shp/city.shp shp/dong.shp
 
 # Generates GeoJSON from TopoJSON
 geojson topojson/regions.topojson -o geojson/ 
